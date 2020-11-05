9e7b7f59128595c259a8c1bd6a00ecca5014b0ea
Nguyen Le Ly Bang <bangjdev@gmail.com>
2020-11-06 00:35:10 +0100

    Update redis setting

--- online_judger_backend/prod_settings.py
+++ online_judger_backend/prod_settings.py
@@ -54,7 +54,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": os.environ.get('REDIS_URL')
+            "hosts": [os.environ.get('REDIS_URL')]
         },
     },
 }
